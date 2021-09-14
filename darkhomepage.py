b5da0fe3967bdf1ab5580162b55dc3b13b985fd7
xubo23 <xubo23@meituan.com>
2021-09-14 17:17:00 +0800

    初始化内容

--- darkhomepage.py
+++ darkhomepage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keep_run.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dark-homepage.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
